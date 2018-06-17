026c5a3e7d78c1f74d785bcce7c7bac05a6fbf0e
Laksh Singla <lakshsingla@gmail.com>
2018-06-17 10:27:05 +0530

    Compilation and linking complete, runtime error

--- utils.cpp
+++ utils.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 using namespace UTILS;
 
-    float randomize0to1() {
+    float UTILS::randomize0to1() {
         srand(time(NULL));
         float random = rand() % 10000;
         random /= 10000;
         return random;
     }
     //Includes a-z and <space>
-    char randomCharacter(){
+    char UTILS::randomCharacter(){
         int random = (int)(floor(UTILS::randomize0to1() * (26 + 1))); // 27 options + 1
         if(random >= 0 && random <= 25) return 'A' + random ;
         else return ' ';
